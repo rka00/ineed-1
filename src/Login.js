ea90fe006b0e1a7dd32877e39f0de1c1219452d6
rka00 <rkfka1348@gmail.com>
2023-10-14 20:53:05 +0900

    modify(입력)

--- src/Login.js
+++ src/Login.js
@@ -7,13 +7,31 @@ import { ReactComponent as GoogleLogin} from "./components/svg/google_login.svg"
 import { useNavigate } from "react-router-dom";
 import './Login.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useGoogleLogin } from '@react-oauth/google'
 
 
 
 export default function Login() {
+
+const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleLogin = () => {
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // 이후에 필요한 로그인 로직을 추가하세요.
+  }
+
 
     const googleSocialLogin = useGoogleLogin({
         onSuccess: (codeResponse) => console.log(codeResponse),
